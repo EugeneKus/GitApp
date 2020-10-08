379c61130dc6452a3697d6e93c0b9ebb2312bfe7
EugeneKus <Yuku1945@gmail.com>
2020-10-08 16:32:51 +0300

    Initial Commit

---
+++ gitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitApp
+//
+//  Created by Eugene Kus on 08.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
